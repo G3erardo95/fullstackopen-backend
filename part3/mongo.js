90850b0730646a18d082c6a60544666ff3119aa3
Gerardo <gerardo.dbl@gmail.com>
2022-05-10 11:11:11 +0200

    exercise 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://R3gold:${password}@myfirstcluster.lwukx.mongodb.net/phonebook?retryWrites=true&w=majority
+
+  `;
+mongoose.connect(url);
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+});
+
+const Person = mongoose.model("Person", phoneSchema);
+
+if (process.argv.length > 3) {
+ const name = process.argv[3];
+ const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+    date: new Date(),
+  });
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log(`Phonebook:`)
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
