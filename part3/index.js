a430e5b5c2728cb114c5edf673615cc83188893a
Gerardo <gerardo.dbl@gmail.com>
2022-05-12 17:04:09 +0200

    exercise 3.22

--- part3/index.js
+++ part3/index.js
@@ -1,76 +1,76 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
-app.use(morgan("tiny"));
-morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   switch (error.name) {
-    case "CastError":
-      return response.status(400).send({ error: "malformatted id" });
-      break;
-    case "ValidationError":
-      return response.status(400).json({ error: error.message });
-      break;
-    case "ParallelSaveError":
-      return response
-        .status(409)
-        .send({ error: "the instance of this document is already saving" });
-      break;
-    case "MongooseError":
-      return response
-        .status(500)
-        .send({ error: "Mongoose generic error happened" });
-    default:
-      return response.status(500).send({ error: "generic error" });
+  case 'CastError':
+    return response.status(400).send({ error: 'malformatted id' })
+    break
+  case 'ValidationError':
+    return response.status(400).json({ error: error.message })
+    break
+  case 'ParallelSaveError':
+    return response
+      .status(409)
+      .send({ error: 'the instance of this document is already saving' })
+    break
+  case 'MongooseError':
+    return response
+      .status(500)
+      .send({ error: 'Mongoose generic error happened' })
+  default:
+    return response.status(500).send({ error: 'generic error' })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-const Person = require("./models/person");
+const Person = require('./models/person')
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/info", (request, response) => {
+app.get('/api/info', (request, response) => {
   Person.estimatedDocumentCount((err, count) => {
     response.send(`<p>Phonebook has info for ${count} people</p>  
-    ${new Date()}`);
-  });
-});
+    ${new Date()}`)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", morgan(":body"), (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', morgan(':body'), (request, response, next) => {
+  const body = request.body
   // const existingNames = persons.map((person) => (names = person.name));
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name or number missing",
-    });
+      error: 'name or number missing',
+    })
     // } else if (existingNames.includes(body.name)) {
     //   return response.status(400).json({
     //     error: "name must be unique",
@@ -81,48 +81,48 @@ app.post("/api/persons", morgan(":body"), (request, response, next) => {
     name: body.name,
     number: body.number,
     date: new Date(),
-  });
+  })
 
   // persons = persons.concat(person);
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // const generateId = () => {
 //   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
 //   return maxId + 1;
 // };
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { number } = request.body
 
   Person.findByIdAndUpdate(
     request.params.id,
     { number },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/models/person.js
+++ part3/models/person.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
@@ -12,30 +12,30 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const phoneSchema = new mongoose.Schema({
-    name: { type: String, minlength: 3, required: true },
-    number: {
-      type: String,
-      validate: {
-        validator: function(v) {
-          return /\d{2,3}-\d{6,8}/.test(v);
-        },
-        message: props => `number ${props.value} doesn't follow the correct scheme [xx(x)-xxxxxxxx]!`
+const phoneSchema = new mongoose.Schema({
+  name: { type: String, minlength: 3, required: true },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,8}/.test(v)
       },
-      required: true,
+      message: props => `number ${props.value} doesn't follow the correct scheme [xx(x)-xxxxxxxx]!`
     },
-    date: {
-      type: Date,
-      required: true,
-    },
-  });
-  
-  phoneSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  });
-  
-  module.exports = mongoose.model('Person', phoneSchema)
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+})
+
+phoneSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', phoneSchema)
