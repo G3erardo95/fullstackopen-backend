474f293ec881e1f65c6bf6e448a37161068a0d05
Gerardo <gerardo.dbl@gmail.com>
2022-07-21 17:21:18 +0200

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,8 +4,17 @@ const dummyBlog = "Hi! There is no blog here! Try adding one right now!"
   if (blogs.length === 0) {
     return blogs.push(dummyBlog)
   };
-}
+};
+
+const totalLikes = (blogs) => {
+  const getSumByKey = (arr, key) => {
+    return arr.reduce((accumulator, current) => accumulator + Number(current[key]), 0)}
+
+    return blogs.length === 0
+    ? 0
+    : getSumByKey(blogs, 'likes')
+  }
 
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
